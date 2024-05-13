d2eddfcc8b754d82265f40f15f6d58c2c40db7c6
Nick Westbrook <ntw3001@gmail.com>
2024-05-13 22:29:01 +0900

    Caesar Cipher

---
+++ CaesarCipher.rb
@@ -0,0 +1,30 @@
+def caesar_cipher(string, shift)
+  letters = string.split("")
+  alphab = ("a".."z").to_a
+  alphab_large = ("A".."Z").to_a
+  alphab_rotated = alphab.rotate(-shift)
+  alphab_large_rotated = alphab_large.rotate(-shift)
+  enciphered = ""
+  letters.each do |letter|
+    if alphab_rotated.include? letter
+      i = alphab_rotated.index(letter)
+      enciphered << alphab[i]
+    elsif alphab_large_rotated.include? letter
+      i = alphab_large_rotated.index(letter)
+      enciphered << alphab_large[i]
+    else
+      enciphered << letter
+    end
+  end
+  puts enciphered
+end
+
+puts "Speak, champion"
+sentence = gets.chomp
+sleep(0.5)
+puts "Select a number, chosen one"
+number = gets.chomp.to_i
+sleep(0.5)
+puts "I have prepared your missive"
+sleep(0.5)
+caesar_cipher(sentence, number)
